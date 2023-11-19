1d99317f1084f784fe32a48708dafd4090006fb7
Pablo Heidenreich <heidenreich20@hotmail.com>
2023-11-19 05:43:41 -0300

    minor fixes

--- src/Components/Reservations.tsx
+++ src/Components/Reservations.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import '../Calendar.css';
 import { Toaster, toast } from 'sonner'
 import { format } from 'date-fns';
 
