6145e38612470e7e10160a2fab5b1ff329d7d389
Pablo Heidenreich <heidenreich20@hotmail.com>
2023-11-19 13:05:12 -0300

    improved SEO

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,10 +1,10 @@
-import logo from '../assets/logo.png'
+import logo from '../assets/logo-medium.webp'
 
 const Footer = () => {
   return (
     <footer className="footer footer-center p-10 bg-alt text-main-text">
       <aside>
-        <img className='w-16 aspect-square' src={logo} alt="company logo" />
+        <img className='w-16 aspect-[1.2/1]' src={logo} alt="company logo" />
         <p>Sapphire Bistro<br />Redefining Culinary Excellence</p>
         <p>Copyright © 2023 - All right reserved</p>
       </aside>
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,18 +4,18 @@ const Navbar = () => {
   return (
     <nav className='z-20 w-full px-8 text-main-text gap-5 backdrop-blur-sm bg-base-100/[0.7] fixed top-0 flex justify-between py-5'>
       <div className='flex items-center gap-2'>
-        <img className='w-8 h-8' src={logo} alt="company logo" />
+        <img className='w-8 aspect-[1.2/1]' src={logo} alt="company logo" />
         <p className='font-heading md:block hidden text-text font-bold'>Sapphire Bistro</p>
       </div>
       <ul className="flex items-center gap-6 md:gap-12 md:text-base text-sm font-heading justify-end">
         <li className="relative">
-          <a href="#menu">Menu</a>
+          <a aria-label='menu' href="#menu">Menu</a>
         </li>
         <li className="text-secondary relative">
-          <a href="#reservations">Reservations</a>
+          <a aria-label='reservations' href="#reservations">Reservations</a>
         </li>
         <li className="relative">
-          <a href="#carrousel">About us</a>
+          <a aria-label='about us' href="#carrousel">About us</a>
         </li>
       </ul>
     </nav>
